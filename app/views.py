cec47e8511d38374f1b3fa58a2d6bfd854af3a1a
William Chung <118134309+WLL1am@users.noreply.github.com>
2024-09-09 14:18:41 -0700

    product category links now working

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
+from urllib import request
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 def home(request):
     return render(request, "app/home.html")
+
+class CategoryView(View):
+    def get(self, request, val):
+        return render(request, "app/category.html", locals())
+    
